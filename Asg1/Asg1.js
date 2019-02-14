74982db0d11ec6e97d995d381bf7ee4b452fbfab
Julien <julienrios1@gmail.com>
2019-02-14 18:16:09 -0500

    Assignment Folders

---
+++ Asg1/Asg1.js
@@ -0,0 +1,12 @@
+var MyNumber = 8
+function double(){
+var execute = MyNumber*2;
+return execute;
+}
+console.log(double());
+
+function clone(MyNum){
+    var x = MyNum*2;
+    return x;
+};
+console.log(clone(4))
