5a64312af769628e48a88187fcff79664599ef0d
Julien Rios-Nosowski <jriosnosowski@cpso.on.ca>
2019-02-25 11:02:21 -0500

    Redoing Assignment

---
+++ Asg4/RemakeAsg4.js
@@ -0,0 +1,48 @@
+var faveColors = ['pink', 'purple', 'red', 'blue']
+var familyAge = [22, 27, 56, 63, 39]
+var coins = ['heads', 'heads', 'tails', 'heads', 'tails']
+var artists = ['florence', 'emma ruth', 'Gwen Stefani', 'worlds end girlfriend']
+
+var words = {
+    food: 'sustenance',
+    music: 'joy in sound',
+    computer: 'entertainment machine',
+}
+var movies = {
+    ScottPilgrim: 2008,
+    CoCo: 2018,
+    FantasticFox: 2012,
+}
+var cities = {
+    Toronto: 20000000,
+    Montreal: 10000000,
+    NYC: 100000000,
+}
+var familyAges = {
+    Julien: 22,
+    Jack: 27,
+    Tomek: 39,
+    Papa: 60,
+    Colston: 53,
+}
+
+// Excercise 1
+
+// console.log(coins)
+// console.log(faveColors[0])
+// console.log(familyAge.sort())
+// familyAge.push(0)
+// console.log(familyAge.sort())
+// console.log(movies.CoCo)
+
+// Excercise 2
+
+var lastArray = x => {
+    return x[x.length-1]
+}
+// console.log(lastArray(faveColors))
+// cities.NewOrleans = 500000
+// console.log(cities)
+// coins.reverse()
+// console.log(coins)
+console.log(cities.Toronto)
