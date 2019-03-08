b21ef24a877f2a89311d899d1f075367d6234830
Julien Rios <julienrios@Juliens-MacBook-Pro.local>
2019-03-07 19:13:03 -0500

    Assignment

--- Excercise14/Excercise14.js
+++ Excercise14/Excercise14.js
@@ -1,40 +1,55 @@
-var ballots = [{1: 'Smudge', 2: 'Tigger', 3: 'Simba'},
-           {1: 'Bella', 2: 'Lucky', 3: 'Tigger'},
-           {1: 'Bella', 2: 'Boots', 3: 'Smudge'},
-           {1: 'Boots', 2: 'Felix', 3: 'Bella'},
-           {1: 'Lucky', 2: 'Felix', 3: 'Bella'},
-           {1: 'Smudge', 2: 'Simba', 3: 'Felix'}]
+// var ballots = [{1: 'Smudge', 2: 'Tigger', 3: 'Simba'},
+//            {1: 'Bella', 2: 'Lucky', 3: 'Tigger'},
+//            {1: 'Bella', 2: 'Boots', 3: 'Smudge'},
+//            {1: 'Boots', 2: 'Felix', 3: 'Bella'},
+//            {1: 'Lucky', 2: 'Felix', 3: 'Bella'},
+//            {1: 'Smudge', 2: 'Simba', 3: 'Felix'}]
 
-var bella = 0
-var lucky = 0
-var tigger = 0
-var simba = 0
-var felix = 0
-var smudge = 0 
-var boots = 0
+// var bella = 0
+// var lucky = 0
+// var tigger = 0
+// var simba = 0
+// var felix = 0
+// var smudge = 0 
+// var boots = 0
 
-ballots.forEach(a=> {
-    if (a[1]== 'Smudge'){ smudge+=3}
-    if (a[1]=='Bella'){bella+=3}
-    if (a[1]=='Boots'){boots+=3}
-    if (a[1]=='Lucky'){lucky+=3}
+// ballots.forEach(a=> {
+//     if (a[1]== 'Smudge'){ smudge+=3}
+//     else if (a[1]=='Bella'){bella+=3}
+//     else if (a[1]=='Boots'){boots+=3}
+//     else if (a[1]=='Lucky'){lucky+=3}
 
-    if (a[2]=='Tigger'){tigger+=2}
-    if (a[2]=='Lucky'){lucky+=2}
-    if (a[2]=='Boots'){boots+=2}
-    if (a[2]=='Felix'){felix+=2}
-    if (a[2]=='Simba'){simba+=2}
+//     if (a[2]=='Tigger'){tigger+=2}
+//     if (a[2]=='Lucky'){lucky+=2}
+//     if (a[2]=='Boots'){boots+=2}
+//     if (a[2]=='Felix'){felix+=2}
+//     if (a[2]=='Simba'){simba+=2}
 
-    if (a[3]=='Smudge'){smudge+=1}
-    if (a[3]=='Bella'){bella+=1}
-    if (a[3]=='Boots'){boots+=1}
-    if (a[3]=='Lucky'){lucky+=1}
+//     if (a[3]=='Smudge'){smudge+=1}
+//     if (a[3]=='Bella'){bella+=1}
+//     if (a[3]=='Boots'){boots+=1}
+//     if (a[3]=='Lucky'){lucky+=1}
+// })
+
+// console.log(bella)
+// console.log(boots)
+// console.log(smudge)
+// console.log(tigger)
+// console.log(simba)
+// console.log(lucky)
+// console.log(felix)
+
+var words = ["Totam", "ut", "odit","quis", "Maiores", "unde", "EX", "EST", "corporis"]
+
+words.forEach(a => {
+    if (a.length>4 && a == a.toLowerCase()){
+        console.log('long and lowercase')
+    }
+    else if (a.length>4){
+        console.log('long')
+    }
+    else if (a == a.toLowerCase()){
+        console.log('lowercase')
+    }
+    else {console.log(a)}
 })
-
-console.log(bella)
-console.log(boots)
-console.log(smudge)
-console.log(tigger)
-console.log(simba)
-console.log(lucky)
-console.log(felix)
