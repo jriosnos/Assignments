63b5b6e2b36cc580c5e187691c5e931b84f50db8
Julien Rios <julienrios@Juliens-MacBook-Pro.local>
2019-03-11 12:54:14 -0400

    Complete

--- ObjectOrient/Bank.js
+++ ObjectOrient/Bank.js
@@ -6,8 +6,16 @@ class BankAccount {
     deposit(n){
         this.balance+= n
     }
+    withdraw (n){
+        this.balance-= n
+    }
+    gainInterest(){
+        this.balance*= this.interestRate
+    }
 }
 
-var julien = new BankAccount(10000, 0.02)
+var julien = new BankAccount(10000, 1.02)
 julien.deposit(4000)
+julien.withdraw(2000)
+julien.gainInterest()
 console.log(julien)
