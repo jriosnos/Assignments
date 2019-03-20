7fa8a0d4a432666f61d8de074e77872e57f2909a
Julien Rios <julienrios@Juliens-MacBook-Pro.local>
2019-03-20 15:34:04 -0400

    Ajax work

--- API/ajax.js
+++ API/ajax.js
@@ -1,8 +1,15 @@
 document.addEventListener("DOMContentLoaded", function(){
+    var myButton = document.getElementById('button')
+    myButton.addEventListener('click', function(){
     $.ajax({
-        url: https://bitmaker-monsters-api.herokuapp.com/monsters.json
-        method: 'GET'
+        url: 'https://bitmaker-monsters-api.herokuapp.com/monsters.json',
+        method: 'GET',
         dataType: 'json'
+    }).done((responseData) => {
+        console.log('Request successfully sent')
+        console.log(responseData)
+    })
     })
     
-})
+    
+    })
